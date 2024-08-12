9510aeaed416b96db008eefa1031d8c434fc7fb9
SergioGala <sergiogalafdz@gmail.com>
2024-08-12 21:35:05 +0000

    first commit

--- src/datastructures.py
+++ src/datastructures.py
@@ -7,53 +7,50 @@ update this file to implement the following already declared methods:
 - get_member: Should return a member from the self._members list
 """
 from random import randint
-init_members=[
-        {  #id:self._generated_Id(),
-           "name": "Jhon",
-           #"last_name": "Jackson",
-           "age": [33],
-           "lucky_numbers": [7,13,22]
-        },
-        {  #id:self._generated_Id(),
-           "name": "Jane",
-           #"last_name": "Jackson",
-           "age": [35],
-           "lucky_numbers": [10,14,3]
-        },
-        {  #id:self._generated_Id(),
-           "name": "Jimmy",
-           #"last_name": "Jackson",
-           "age": [5],
-           "lucky_numbers": [1]
-        },
-        ]
 class FamilyStructure:
-    def __init__(self, last_name, members=[]):
+    def __init__(self, last_name):
         self.last_name = last_name
         self._next_id = 1
-        # example list of members
-        self._members = []
-        [self.add_member(member) for member in members]
+        self._members = [
+            {
+                "id": self._generate_id(),
+                "first_name": "John",
+                "last_name": last_name,
+                "age": 33,
+                "lucky_numbers": [7, 13, 22]
+            },
+            {
+                "id": self._generate_id(),
+                "first_name": "Jane",
+                "last_name": last_name,
+                "age": 35,
+                "lucky_numbers": [10, 14, 3]
+            },
+            {
+                "id": self._generate_id(),
+                "first_name": "Jimmy",
+                "last_name": last_name,
+                "age": 5,
+                "lucky_numbers": [1]
+            }
+        ]
 
-    # read-only: Use this method to generate random members ID's when adding members into the list
-    def _generateId(self):
-       generated_id = self._next_id
-       self._next_id += 1
-       return generated_id
+    def _generate_id(self):
+        generated_id = self._next_id
+        self._next_id += 1
+        return generated_id
 
     def add_member(self, member):
-        # fill this method and update the return
-
-        pass
+        if 'id' not in member:
+            member['id'] = self._generate_id()
+        member['last_name'] = self.last_name
+        self._members.append(member)
 
     def delete_member(self, id):
-        # fill this method and update the return
-        pass
+        self._members = [member for member in self._members if member['id'] != id]
 
     def get_member(self, id):
-        # fill this method and update the return
-        pass
+        return next((member for member in self._members if member['id'] == id), None)
 
-    # this method is done, it returns a list with all the family members
     def get_all_members(self):
         return self._members
